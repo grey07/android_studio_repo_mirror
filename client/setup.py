26f707588f9d487102070f72d4d84f64407aad9b
grey07 <carlconnallypetty@gmail.com>
2016-12-23 11:48:26 -0600

    Added shbangs to all python files, added the initial setup for the client

---
+++ client/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Android Repository Mirror',
+      version='1.0',
+      description='This is the offline componenet capable of mirroring android studio repos.',
+      author='grey',
+      url='https://github.com/grey07/android_studio_repo_mirror',
+      packages=['cherrypy >= 8.1.3'],
+     )
